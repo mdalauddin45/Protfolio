7da1d9abb2bcc79c5e819fd7e71f68275c1225c2
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-12-10 13:35:31 +0600

    host

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../pages/shared/Footer";
 import Navbar from "../pages/shared/Navbar";
-import "./Main.css";
 
 const Main = () => {
   return (
