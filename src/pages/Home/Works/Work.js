a4b1a753cdb4436e0a412786f1ddfc06bdb6e534
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-12-10 11:36:24 +0600

    dynamic link add

--- src/pages/Home/Works/Work.js
+++ src/pages/Home/Works/Work.js
@@ -1,15 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import img1 from "../../../assects/Korai.jpg";
 import { LinkIcon } from "@heroicons/react/24/solid";
-const Work = () => {
+const Work = ({ project }) => {
+  console.log(project);
   return (
     <div aria-label="View Item">
       <div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl ">
         <img
           className="object-cover w-full h-56 md:h-64 xl:h-80"
-          src={img1}
-          alt=""
+          src={project?.image}
+          alt="..."
         />
         <div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100 ">
           <a href="https://korai-cox.web.app/" target="_blank">
--- src/pages/Home/Works/Works.js
+++ src/pages/Home/Works/Works.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import Work from "./Work";
 
 function Works() {
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    fetch("projects.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setProjects(data);
+      });
+  }, []);
+  console.log(projects);
   return (
     <div>
       <div className="text-center">
@@ -13,7 +24,9 @@ function Works() {
       </div>
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <div className="grid gap-6 row-gap-5 mb-8 lg:grid-cols-4 sm:row-gap-6 sm:grid-cols-2">
-          <Work />
+          {projects.map((project) => (
+            <Work project={project} key={project.id}></Work>
+          ))}
         </div>
       </div>
     </div>
