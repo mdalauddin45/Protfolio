280ef2fb5aa6d7c87be62ba37c0cf133fc35c39f
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-12-11 13:13:08 +0600

    picture add

--- src/pages/Home/HeaderSection/HeaderSection.js
+++ src/pages/Home/HeaderSection/HeaderSection.js
@@ -25,7 +25,7 @@ function HeaderSection() {
             <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
               <Link
                 rel="noopener noreferrer"
-                to="#"
+                to="/about"
                 className=" text-lg font-semibold rounded"
               >
                 <PrimaryButton classes="px-8 py-3 text-lg font-semibold rounded-full">
--- src/pages/Home/Works/ProjectDetails.js
+++ src/pages/Home/Works/ProjectDetails.js
@@ -4,10 +4,40 @@ import PrimaryButton from "../../../components/Button/PrimaryButton";
 
 const ProjectDetails = () => {
   const projects = useLoaderData();
-  //   console.log(projects);
-  const { image, name, clientside, liveside, serverside } = projects;
+  console.log(projects.thumble);
+  const { image, name, clientside, liveside, serverside, thumble } = projects;
+  const { image1, image2, image3, image4, image5 } = thumble;
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <section className="py-6  dark:text-gray-50">
+        <div className="container grid grid-cols-2 gap-4 p-4 mx-auto md:grid-cols-4">
+          <img
+            src={image1}
+            alt=""
+            className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 dark:bg-gray-500 aspect-square"
+          />
+          <img
+            alt=""
+            className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square"
+            src={image2}
+          />
+          <img
+            alt=""
+            className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square"
+            src={image3}
+          />
+          <img
+            alt=""
+            className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square"
+            src={image4}
+          />
+          <img
+            alt=""
+            className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square"
+            src={image5}
+          />
+        </div>
+      </section>
       <div className="flex flex-col max-w-screen-lg overflow-hidden bg-white border rounded shadow-sm lg:flex-row sm:mx-auto">
         <div className=" lg:w-1/2">
           <img
